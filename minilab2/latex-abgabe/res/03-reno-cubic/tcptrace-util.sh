41fcf0f7978011707326a4b1f48ccfef6c2829a3
Sunil16666 <79449240+Sunil16666@users.noreply.github.com>
2026-07-26 20:23:45 +0200

    chore(tcp-util): format tcptrace-util.sh

--- minilab2/latex-abgabe/res/03-reno-cubic/tcptrace-util.sh
+++ minilab2/latex-abgabe/res/03-reno-cubic/tcptrace-util.sh
@@ -4,5 +4,9 @@ set -eux
 apt-get update && apt-get install -y tcptrace
 
 for algorithm in reno cubic; do
-  tcptrace -n -l -r -W -N -R -z -o2 --output_dir=/analysis --output_prefix="${algorithm}-" "/data/tcp_${algorithm}.pcap" > "/analysis/${algorithm}-summary.txt"
+	tcptrace -n -l -r -W -N -R -z -o2 \
+		--output_dir=/analysis \
+		--output_prefix="${algorithm}-" \
+		"/data/tcp_${algorithm}.pcap" \
+		>"/analysis/${algorithm}-summary.txt"
 done
